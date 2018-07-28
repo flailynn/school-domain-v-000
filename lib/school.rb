01caa31d77fa15cda776eaa4a1ab3fcf56519401
Anita Beauchamp <amadamspr@gmail.com>
2018-07-28 03:14:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,7 @@ class School
   end
 
   def add_student(student_name, grade)
-    roster[grade] << student_name
+    roster[grade] = [] if roster[grade].nil?
+    roster[grade]  << student_name
   end
 end
